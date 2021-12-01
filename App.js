be5b04a6331495b8dcfc83d28c5ed3d6412df4d8
Thales Zago <thaleszago@outlook.com>
2021-12-01 00:01:58 -0300

    adding the four mathematical operations

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [primeiro, setPrimeiro] = useState(0);
@@ -8,30 +8,37 @@ export default function App() {
   const x = parseFloat(primeiro);
   const y = parseFloat(segundo);
 
+  const subtracao = x - y;
+  const multiplicacao = x * y;
+  const divisao = x / y;
   const soma = x + y;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Calculadora</Text>
-      <Text>
-        {" "}
-        Coloque os números que deseja somar e o app irá somar automaticamente{" "}
-      </Text>
-
-      <TextInput
-        keyboardType="decimal-pad"
-        style={styles.input}
-        value={primeiro}
-        onChangeText={setPrimeiro}
-      />
-      <TextInput
-        keyboardType="decimal-pad"
-        style={styles.input}
-        value={segundo}
-        onChangeText={setSegundo}
-      />
-      <Text style={styles.subtitle}> Resultado </Text>
-      <Text> {soma} </Text>
+      <View style={styles.card}>
+        <Text style={styles.title}> Calculadora</Text>
+        <Text style={styles.helper}>
+          Coloque os números que deseja somar e o app irá somar automaticamente
+        </Text>
+        <TextInput
+          keyboardType="decimal-pad"
+          style={styles.input}
+          value={primeiro}
+          onChangeText={setPrimeiro}
+        />
+        <TextInput
+          keyboardType="decimal-pad"
+          style={styles.input}
+          value={segundo}
+          onChangeText={setSegundo}
+        />
+        <Button onClick={somar()}> clique me</Button>
+        <Text style={styles.subtitle}> Resultado </Text>
+        <Text style={styles.result}> Soma: {soma} </Text>;
+        <Text style={styles.result}> Subtração: {subtracao} </Text>
+        <Text style={styles.result}> Multiplicação: {multiplicacao} </Text>
+        <Text style={styles.result}> Divisão: {divisao} </Text>
+      </View>
     </View>
   );
 }
@@ -39,27 +46,52 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fafafa",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fafafa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    width: 400,
+    height: 700,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    borderRadius: 5,
   },
   title: {
     fontSize: 36,
-    fontWeight: "100",
+    fontWeight: '100',
+    color: '#ddd',
+  },
+  helper: {
+    fontSize: 16,
+    fontWeight: '100',
+    color: '#ddd',
+    textAlign: 'center',
+    alignSelf: 'center',
   },
   input: {
-    textAlign: "center",
-    alignSelf: "stretch",
+    textAlign: 'center',
+    alignSelf: 'center',
     height: 50,
-    backgroundColor: "#DDD",
+    backgroundColor: '#DDD',
     marginTop: 20,
     marginHorizontal: 36,
     fontSize: 36,
-    fontWeight: "100",
+    fontWeight: '100',
   },
   subtitle: {
     marginTop: 20,
-    fontWeight: "bold",
-    fontSize: 16,
+    fontWeight: 'bold',
+    fontSize: 36,
+    color: '#ddd',
+  },
+  result: {
+    textAlign: 'start',
+    alignSelf: 'start',
+    marginTop: 20,
+    fontWeight: '100',
+    fontSize: 30,
+    color: '#ddd',
   },
 });
--- README.md
+++ README.md
@@ -1,30 +1,37 @@
 # react-calculadora
+
 Testing new knowledges using React Native.
 
 ## Run Locally
 
 Clone the project
-```bash 
-git clone https://github.com/ThalesZago/react-calculadora.git" 
+
+```bash
+git clone https://github.com/ThalesZago/react-calculadora.git"
 ```
 
 Go to the root folder
-```bash 
-cd react-calculadora 
+
+```bash
+cd react-calculadora
 ```
 
 Install the modules
+
 ```bash
 yarn install
 ```
 
 Run the project
+
 ```bash
 yarn start
 ```
 
+Yarn will ask you to instal Expo-cli, type Y
+
 To stop the running project, press
+
 ```bash
 ctrl + c
 ```
-
